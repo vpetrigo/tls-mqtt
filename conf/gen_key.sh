afcbdaf8cd5aaf21b0d0e6a9622b0fe9dc6c8b41
Vladimir Petrigo <taenaru@gmail.com>
2018-06-25 12:08:35 +0300

    Update gen_key.sh

--- conf/gen_key.sh
+++ conf/gen_key.sh
@@ -4,18 +4,18 @@ set -e
 set -o pipefail
 
 # Generate CA certificate
-openssl req -new -nodes -x509 -days 365 -extensions v3_ca -keyout ca.key -out ca.crt -subj "/C=RU/ST=Saint Petersburg/L=Saint Petersburt/O=Test Company/OU=IT Department/CN=caroot"
+openssl req -new -nodes -x509 -days 365 -extensions v3_ca -keyout ca.key -out ca.crt -subj "/C=RU/ST=Saint Petersburg/L=Saint Petersburg/O=Test Company/OU=IT Department/CN=caroot"
 
 # Generate server key
 openssl genrsa -out server.key 2048
 # Generate signing request
-openssl req -out server.csr -key server.key -new -subj "/C=RU/ST=Saint Petersburg/L=Saint Petersburt/O=Test Company/OU=IT Department/CN=server"
+openssl req -out server.csr -key server.key -new -subj "/C=RU/ST=Saint Petersburg/L=Saint Petersburg/O=Test Company/OU=IT Department/CN=server"
 # Sign server key with the CA key
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 
 
 # Generate client key
 openssl genrsa -out client.key 2048
 # Generate signing request
-openssl req -out client.csr -key client.key -new -subj "/C=RU/ST=Saint Petersburg/L=Saint Petersburt/O=Test Company/OU=IT Department/CN=client"
+openssl req -out client.csr -key client.key -new -subj "/C=RU/ST=Saint Petersburg/L=Saint Petersburg/O=Test Company/OU=IT Department/CN=client"
 # Sign client key with the CA key
 openssl x509 -req -in client.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out client.crt -days 365
